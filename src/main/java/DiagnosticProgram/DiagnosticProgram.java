40e3bb12e348226942aa4c5f2a1a7d77e579ce7c
cr618 <cyprien.roche18@imperial.ac.uk>
2019-12-15 18:31:19 +0000

    refactor diagnostic program executeOp method

--- src/main/java/DiagnosticProgram/DiagnosticProgram.java
+++ src/main/java/DiagnosticProgram/DiagnosticProgram.java
@@ -2,6 +2,7 @@ package DiagnosticProgram;
 
 import static DiagnosticProgram.Op.*;
 
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,13 +10,13 @@ public class DiagnosticProgram {
 
   private final int[] program;
   private int PC;
-  private Scanner scanner;
   private int code;
+  private Scanner scanner = new Scanner(System.in);
+  private PrintStream printStream = System.out;
 
   public DiagnosticProgram(final int[] program) {
     this.program = Arrays.copyOf(program, program.length);
     this.PC = 0;
-    this.scanner = new Scanner(System.in);
   }
 
   public int[] getProgram() {
@@ -24,11 +25,11 @@ public class DiagnosticProgram {
 
   public DiagnosticProgram execute() {
     while (PC + 1 < program.length) {
-      int code = program[PC];
-      if (code == HALT) {
+      int opcode = program[PC];
+      if (opcode == HALT) {
         return this;
       }
-      this.code = code;
+      code = opcode;
       PC += executeOp();
     }
     return this;
@@ -37,23 +38,31 @@ public class DiagnosticProgram {
   private int executeOp() {
     switch (getOpcode()) {
       case INP -> {
-        program[getPositionOfArg(1)] = scanner.nextInt();
+        setArg(1, scanner.nextInt());
         return 2;
       }
       case OUT -> {
-        System.out.println(program[getPositionOfArg(1)]);
+        printStream.println(getArg(1));
         return 2;
       }
-    }
-
-    int a = program[getPositionOfArg(1)];
-    int b = program[getPositionOfArg(2)];
-    program[getPositionOfArg(3)] = switch (getOpcode()) {
-      case ADD -> a + b;
-      case MUL -> a * b;
+      case ADD -> {
+        setArg(3, getArg(1) + getArg(2));
+        return 4;
+      }
+      case MUL -> {
+        setArg(3, getArg(1) * getArg(2));
+        return 4;
+      }
       default -> throw new IllegalArgumentException(illegalArgMessage(code));
-    };
-    return 4;
+    }
+  }
+
+  private void setArg(int i, int value) {
+    program[getPositionOfArg(i)] = value;
+  }
+
+  private int getArg(int i) {
+    return program[getPositionOfArg(i)];
   }
 
   private int getOpcode() {

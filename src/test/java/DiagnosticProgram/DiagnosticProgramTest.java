f712e7e3bc03330b6d49e043633b0e39b1e97813
cr618 <cyprien.roche18@imperial.ac.uk>
2019-12-15 19:06:28 +0000

    add new tests for part two

--- src/test/java/DiagnosticProgram/DiagnosticProgramTest.java
+++ src/test/java/DiagnosticProgram/DiagnosticProgramTest.java
@@ -109,4 +109,74 @@ public class DiagnosticProgramTest {
     new DiagnosticProgram(program3).execute().getProgram();
     assertEquals("239\n", outContent.toString());
   }
+
+  @Test
+  public void jumpsIfFirstParameterIsNonZero() {
+    int[] program1 = {5,1,7,1,0,2,0,99};
+    int[] program2 = {5,0,7,1,0,2,0,99};
+    assertThat(new DiagnosticProgram(program1).execute().getProgram(), is(new int[]{5,1,7,1,0,2,0,99}));
+    assertThat(new DiagnosticProgram(program2).execute().getProgram(), is(new int[]{12,0,7,1,0,2,0,99}));
+  }
+
+  @Test
+  public void jumpsIfFirstParameterIsZero() {
+    int[] program1 = {6,0,7,1,0,2,0,99};
+    int[] program2 = {6,1,7,1,0,2,0,99};
+    assertThat(new DiagnosticProgram(program1).execute().getProgram(), is(new int[]{6,1,7,1,0,2,0,99}));
+    assertThat(new DiagnosticProgram(program2).execute().getProgram(), is(new int[]{13,0,7,1,0,2,0,99}));
+  }
+
+  @Test
+  public void storesOneInPosOfThirdParameterIfFirstParameterIsLessThanSecond() {
+    int[] program1 = {7,3,4,0,99};
+    int[] program2 = {7,4,3,0,99};
+    assertThat(new DiagnosticProgram(program1).execute().getProgram(), is(new int[]{1,3,4,0,99}));
+    assertThat(new DiagnosticProgram(program2).execute().getProgram(), is(new int[]{0,4,3,0,99}));
+  }
+
+  @Test
+  public void storesOneInPosOfThirdParameterIfFirstParameterIsEqualToSecond() {
+    int[] program1 = {8,3,3,0,99};
+    int[] program2 = {8,3,7,0,99};
+    assertThat(new DiagnosticProgram(program1).execute().getProgram(), is(new int[]{1,3,3,0,99}));
+    assertThat(new DiagnosticProgram(program2).execute().getProgram(), is(new int[]{0,3,7,0,99}));
+  }
+
+  @Test
+  public void ifInputEqualEightOutputOneElseOutputZero() {
+    int[] program1 = {3,9,8,9,10,9,4,9,99,-1,8};
+    new DiagnosticProgram(program1).execute();
+    assertEquals("0\n", outContent.toString());
+  }
+
+  @Test
+  public void ifInputLessThanEightOutputOneElseOutputZero() {
+    int[] program1 = {3,9,7,9,10,9,4,9,99,-1,8};
+    new DiagnosticProgram(program1).execute();
+    assertEquals("1\n", outContent.toString());
+  }
+
+
+  @Test
+  public void ifInputEqualEightOutputOneElseOutputZeroImmediateMode() {
+    int[] program1 = {3,3,1108,-1,8,3,4,3,99};
+    new DiagnosticProgram(program1).execute();
+    assertEquals("0\n", outContent.toString());
+  }
+
+  @Test
+  public void ifInputLessThanEightOutputOneElseOutputZeroImmediateMode() {
+    int[] program1 = {3,3,1107,-1,8,3,4,3,99};
+    new DiagnosticProgram(program1).execute();
+    assertEquals("1\n", outContent.toString());
+  }
+
+  @Test
+  public void example4() {
+    int[] program1 = {3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99};
+    new DiagnosticProgram(program1).execute();
+    assertEquals("999\n", outContent.toString());
+  }
 }

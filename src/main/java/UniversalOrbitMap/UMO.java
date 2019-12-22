5012daadc6345460c024c3c6f3dd2c0d2c74ad1d
cr618 <cyprien.roche18@imperial.ac.uk>
2019-12-22 23:03:34 +0100

    add basic implementation to pass first test in Universal Orbit Map

--- src/main/java/UniversalOrbitMap/UMO.java
+++ src/main/java/UniversalOrbitMap/UMO.java
@@ -1,5 +1,21 @@
 package UniversalOrbitMap;
 
+import java.util.Arrays;
+
 public class UMO {
 
+  int count;
+
+  public UMO(String[] strings) {
+    count = 0;
+    Arrays.stream(strings).forEach(this::addOrbit);
+  }
+
+  private void addOrbit(String orbit) {
+    count += 1;
+  }
+
+  public int getOrbitNumber() {
+    return count;
+  }
 }
--- src/test/java/UniversalOrbitMap/UMOTest.java
+++ src/test/java/UniversalOrbitMap/UMOTest.java
@@ -2,6 +2,31 @@ package UniversalOrbitMap;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class UMOTest {
 
+  @Test
+  public void basicOrbit() {
+    UMO umo = new UMO(new String[]{"AAA)BBB"});
+    assertEquals(1, umo.getOrbitNumber());
+  }
+
+  @Test
+  public void example() {
+    UMO umo = new UMO(new String[]{
+        "COM)B",
+        "B)C",
+        "C)D",
+        "D)E",
+        "E)F",
+        "B)G",
+        "G)H",
+        "D)I",
+        "E)J",
+        "J)K",
+        "K)L",
+    });
+    assertEquals(42, umo.getOrbitNumber());
+  }
 }

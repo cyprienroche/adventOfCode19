812a1fe3156b90f3d26c283b583f632f6fa16c64
cr618 <cyprien.roche18@imperial.ac.uk>
2019-12-23 23:20:09 +0100

    add Main function to generate output for orbital transfer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,7 +33,7 @@ public class Main {
 //    PasswordPermutation.PasswordPermutation.execute(printStream);
 //      DiagnosticMain.execute(scanner, printStream);
 //    UMOMain.getOrbits(scanner, printStream);
-    UMOMain.getOrbits(scanner, printStream);
+    UMOMain.getOrbitalTransfer(scanner, printStream);
 
 
     System.out.println("process complete.\nexit...");
